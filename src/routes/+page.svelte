e21537c928eca8097f4c93be82ef746369569829
Afrody <herdianafrody@gmail.com>
2024-06-19 21:43:20 +0700

    Proxy vercel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 
 	const fetchData = async () => {
 		try {
-			const response = await axios.get('http://localhost:52773/api/prototype/patients', {
+			const response = await axios.get('/api/prototype/patients', {
 				headers: {
 					Authorization: `Basic ${token}`
 				}
@@ -25,19 +25,19 @@
 		}
 	};
 
-	// const handleTest = async () => {
-	// 	try {
-	// 		const response = await axios.get('https://dev-hub.zicare.id/api/v1/condition', {
-	// 			headers: {
-	// 				Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTg3OTE1MzAuNDYxNTc5LCJleHAiOjE3MTg3OTUxMzAsImlzcyI6IkludGVyU3lzdGVtcyIsInN1YiI6IlNhdHVzZWhhdCIsInNpZCI6ImRmMUplYTF4K1htL3FMUFQ0TWpNYitPbiIsImFwcCI6Ii9hcGkvdjEvIn0.wE8n8cQM9rk4LR_KSoU9lWTkWdwcKsb53qLHXILOZLvWUvA4jBhoZ7UvcuSQDuyUDZTkV00gZAsyDhqeGnPiANFTMYcD6LoHXYedjsIuuVMS7sSUTGrIZvXV41fw0JLRoHy862CeUnKpPAq8o-bgkYXUrm8uzmK-5vjA1Yww0gQvasWg6w06DNlobi6LRlxxLBOG16-nbfoPdTkW-Y__KLFWbJmGFa1-NGfRaqS89W99DE_2Qozn92hoIsV-NSvjV6RCi3rVcZBvKdPTzkWEmUH6SrZg1FdFuv0sscDD0ZTQjeI_9VQ1Vi34zzDCwWMbgwdoP1QuK5u8_qI3syPs4Q`
-	// 			}
-	// 		});
+	const handleTest = async () => {
+		try {
+			const response = await axios.get('https://dev-hub.zicare.id/api/v1/condition', {
+				headers: {
+					Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTg3OTE1MzAuNDYxNTc5LCJleHAiOjE3MTg3OTUxMzAsImlzcyI6IkludGVyU3lzdGVtcyIsInN1YiI6IlNhdHVzZWhhdCIsInNpZCI6ImRmMUplYTF4K1htL3FMUFQ0TWpNYitPbiIsImFwcCI6Ii9hcGkvdjEvIn0.wE8n8cQM9rk4LR_KSoU9lWTkWdwcKsb53qLHXILOZLvWUvA4jBhoZ7UvcuSQDuyUDZTkV00gZAsyDhqeGnPiANFTMYcD6LoHXYedjsIuuVMS7sSUTGrIZvXV41fw0JLRoHy862CeUnKpPAq8o-bgkYXUrm8uzmK-5vjA1Yww0gQvasWg6w06DNlobi6LRlxxLBOG16-nbfoPdTkW-Y__KLFWbJmGFa1-NGfRaqS89W99DE_2Qozn92hoIsV-NSvjV6RCi3rVcZBvKdPTzkWEmUH6SrZg1FdFuv0sscDD0ZTQjeI_9VQ1Vi34zzDCwWMbgwdoP1QuK5u8_qI3syPs4Q`
+				}
+			});
 
-	// 		console.log(response);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	onMount(() => {
 		handleAuth(username, password);
@@ -50,9 +50,9 @@
 	<!-- Column 1 -->
 	<div class="w-full px-2 mt-10">
 		<div class="bg-gray-100 p-4 rounded shadow">
-			<!-- <button type="button" on:click={handleTest} class="px-2 py-1 rounded bg-red-500 text-white"
+			<button type="button" on:click={handleTest} class="px-2 py-1 rounded bg-red-500 text-white"
 				>Test</button
-			> -->
+			>
 			<h2 class="text-2xl font-bold text-gray-700 mb-5">Data Pasien</h2>
 
 			<table class="w-full">
