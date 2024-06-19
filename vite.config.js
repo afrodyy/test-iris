c8f0fbb34f208811112e508249d2f22947fc3524
Afrody <herdianafrody@gmail.com>
2024-06-19 21:20:03 +0700

    Test

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api/prototype': {
-				target: 'http://localhost:52773',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/prototype/, '/api/prototype')
-			},
-			'/api/v1/condition': {
-				target: 'https://dev-hub.zicare.id',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/v1\/condition/, '/api/prototype/condition')
-			}
-		}
-	}
+	plugins: [sveltekit()]
+	// server: {
+	// 	proxy: {
+	// 		'/api/prototype': {
+	// 			target: 'http://localhost:52773',
+	// 			changeOrigin: true,
+	// 			rewrite: (path) => path.replace(/^\/api\/prototype/, '/api/prototype')
+	// 		},
+	// 		'/api/v1/condition': {
+	// 			target: 'https://dev-hub.zicare.id',
+	// 			changeOrigin: true,
+	// 			rewrite: (path) => path.replace(/^\/api\/v1\/condition/, '/api/prototype/condition')
+	// 		}
+	// 	}
+	// }
 });
